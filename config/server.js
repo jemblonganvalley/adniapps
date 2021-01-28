5c9a8b44bb17935ec6a1c6ff63f6ba439e52b8c9
jemblonganvalley <jemblonganvalley@gmail.com>
2021-01-29 01:18:12 +0700

    upload pertama kali

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f8b2a5502b3accf131e2be15024e6861'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'db1d9537-6668-4f79-8045-ce9b92a89804'
+};
